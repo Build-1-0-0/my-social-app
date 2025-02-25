8ecadd2a0303b94d9726d996e060592c68e7864b
Build-1-0-0 <africancontent807@gmail.com>
2025-02-25 14:46:30 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DataFetcher from './DataFetcher'; // Import the component
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <DataFetcher /> {/* Use the component */}
+            </header>
+        </div>
+    );
+}
+
+export default App;
